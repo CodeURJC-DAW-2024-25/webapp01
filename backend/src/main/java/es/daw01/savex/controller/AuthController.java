196f237ac5f3513905273ad115fafd95aadad8da
CuB1z <cub1zdev@gmail.com>
2025-02-16 14:02:29 +0100

    Feature: Enhance login and registration forms with CSRF protection and update field labels

--- backend/src/main/java/es/daw01/savex/controller/AuthController.java
+++ backend/src/main/java/es/daw01/savex/controller/AuthController.java
@@ -1,14 +1,21 @@
 package es.daw01.savex.controller;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthController {
 
     @GetMapping("/login")
-    public String getLoginPage(Model model) {
+    public String getLoginPage(Model model, HttpServletRequest request) {
+        // Get user CSRF token from login form
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        
+        model.addAttribute("token", token.getToken());
         model.addAttribute("title", "SaveX - Iniciar sesión");
         return "login";
     }
