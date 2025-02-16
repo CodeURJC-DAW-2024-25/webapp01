5bc9d1609163cef6385e58094396ddde39e4d47a
CuB1z <cub1zdev@gmail.com>
2025-02-16 14:03:03 +0100

    Feature: Implement custom error handling with a dedicated error controller and error page template

---
+++ backend/src/main/java/es/daw01/savex/controller/CustomErrorController.java
@@ -0,0 +1,35 @@
+package es.daw01.savex.controller;
+
+import java.util.Map;
+
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private ErrorAttributes errorAttributes = null;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @GetMapping("/error")
+    public String handleError(WebRequest webRequest, Model model) {
+        Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(webRequest, null);
+        Integer status = (Integer) errorAttributes.get("status");
+
+        // Set error page title
+        if (status == 404) model.addAttribute("title", "SaveX - Página no encontrada");
+        else model.addAttribute("title", "SaveX - Error ".concat(status.toString()));
+
+        // Set error page attributes
+        model.addAttribute("status", status);
+        return "error";
+    }
+}
