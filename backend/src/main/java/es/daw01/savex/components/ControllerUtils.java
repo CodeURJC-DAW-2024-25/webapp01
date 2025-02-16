74ef5cff47cd05167edee5cb65b80a13b9d69f9b
CuB1z <cub1zdev@gmail.com>
2025-02-16 20:44:02 +0100

    Fix: Refactor ControllerUtils for validating user is not null

--- backend/src/main/java/es/daw01/savex/components/ControllerUtils.java
+++ backend/src/main/java/es/daw01/savex/components/ControllerUtils.java
@@ -1,7 +1,5 @@
 package es.daw01.savex.components;
 
-import java.sql.Blob;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,6 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
+import es.daw01.savex.model.User;
 import es.daw01.savex.service.UserService;
 
 @Component
@@ -25,21 +24,31 @@ public class ControllerUtils {
     */
     public void addUserDataToModel(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Blob avatar = null;
+        User user = null;
 
         // Check if user is authenticated and add data to the model
         if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
             model.addAttribute("isAuthenticated", true);
 
-            // Retrieve user avatar
-            avatar = this.userService.findByUsername(auth.getName()).get().getAvatar();
+            // Retrieve user data
+            user = this.userService.findByUsername(auth.getName()).get();
         } else {
             model.addAttribute("isAuthenticated", false);
         }
 
-        // Check for null avatar
-        if (avatar == null) model.addAttribute("avatar", DEFAULT_AVATAR);
-        else model.addAttribute("avatar", avatar);
+        // Set avatar attribute
+        if (user == null) {
+            model.addAttribute("avatar", DEFAULT_AVATAR);
+            model.addAttribute("name", auth.getName());
+        } else {
+            // Set avatar attribute
+            if (user.getAvatar() == null) model.addAttribute("avatar", DEFAULT_AVATAR);
+            else model.addAttribute("avatar", user.getAvatar());
+            
+            // Set name attribute
+            if (user.getName().isEmpty()) model.addAttribute("name", user.getUsername());
+            else model.addAttribute("name", user.getName());
+        }
         
         // Add the rest of the data to the model
         model.addAttribute("username", auth.getName());
