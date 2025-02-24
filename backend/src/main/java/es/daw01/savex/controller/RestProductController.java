10f8aaaedaa943731702d06454c00a7e490f62fe
victorrosalejo <v.arroyom.2021@alumnos.urjc.es>
2025-02-24 12:52:19 +0100

    Feat: Refactor search endpoint to return ResponseEntity and update search bar with CSRF token

--- backend/src/main/java/es/daw01/savex/controller/RestProductController.java
+++ backend/src/main/java/es/daw01/savex/controller/RestProductController.java
@@ -1,26 +1,27 @@
 package es.daw01.savex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import es.daw01.savex.service.ApiService;
 
-@Controller
+@RestController
 public class RestProductController {
+
     @Autowired
     private ApiService apiService;
 
     @PostMapping("/search") 
-    public String search(@RequestParam("searchInput") String searchInput, Model model) {
+    public ResponseEntity<String> search(@RequestParam("searchInput") String searchInput) {
         System.out.println("Search input: " + searchInput);
         System.out.println("---------------------------------------------------");
         String apiResponse = apiService.fetchData();
         System.out.println("---------------------------------------------------");
         System.out.println("API response: " + apiResponse);
-        model.addAttribute("apiResponse", apiResponse);
-        return "redirect:/";
+
+        return ResponseEntity.ok(apiResponse);
     }
 }
