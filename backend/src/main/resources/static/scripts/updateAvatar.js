73b4a6d648581b9ee9f7306c2f86a86a338808dc
CuB1z <cub1zdev@gmail.com>
2025-02-25 10:07:26 +0100

    Feat: Allow users to upload images and update with no reloading

--- backend/src/main/resources/static/scripts/updateAvatar.js
+++ backend/src/main/resources/static/scripts/updateAvatar.js
@@ -1,33 +1,36 @@
 
 const CSRF_TOKEN = document.querySelector('meta[name="_csrf"]').content;
 const CSRF_HEADER = document.querySelector('meta[name="_csrf_header"]').content;
+
 const input = document.getElementById('avatar-input');
 const submitButton = document.getElementById('submit-button');
+const avatarImages = document.querySelectorAll('.avatar');
 
-input.addEventListener('change', () => {
+input.addEventListener('change', async () => {
+    // Generate a FormData object
+    const file = input.files[0];
     const formData = new FormData();
-    formData.append('file', input.files[0])
+    formData.append('avatar', file);
+    formData.enctype = 'multipart/form-data';
 
-    fetch('/api/profile/avatar', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            [CSRF_HEADER]: CSRF_TOKEN
-        }
-    })
-    .then(response => {
-        if (!response.ok) throw new Error('Failed to update avatar');
-        return response.json();
-    })
-    .then(data => {
-        document.querySelector('.user-avatar').src = data.avatar;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-})
+    try {
+        await fetch('/api/profile/avatar', {
+            method: 'POST',
+            headers: {
+                [CSRF_HEADER]: CSRF_TOKEN
+            },
+            body: formData
+        })
+    
+        avatarImages.forEach(avatar => {
+            avatar.src = URL.createObjectURL(file);
+        });
+        
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 submitButton.addEventListener('click', () => {
     input.click();
-}
-)
+});
