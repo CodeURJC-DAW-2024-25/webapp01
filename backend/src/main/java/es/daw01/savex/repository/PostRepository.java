b993233a45e1b926a1d3d14d58fd6c1f2900cdad
CuB1z <cub1zdev@gmail.com>
2025-02-26 09:28:10 +0100

    Feat: Add pagination support for retrieving posts ordered by creation date

--- backend/src/main/java/es/daw01/savex/repository/PostRepository.java
+++ backend/src/main/java/es/daw01/savex/repository/PostRepository.java
@@ -2,6 +2,8 @@ package es.daw01.savex.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.daw01.savex.model.Post;
@@ -10,4 +12,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findById(Long id);
     boolean existsById(Long id);
     boolean existsByTitle(String title);
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
