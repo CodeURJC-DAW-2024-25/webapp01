d6443f0412c920a1df6ca3b8f88fd99bc324ba0f
CuB1z <cub1zdev@gmail.com>
2025-03-01 15:03:49 +0100

    Style: Update Popup styling

--- backend/src/main/resources/static/scripts/popup.js
+++ backend/src/main/resources/static/scripts/popup.js
@@ -5,10 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
             const modal = document.getElementById('popup');
             if (modal) {
                 modal.classList.add('show');
-                modal.style.display = 'block';
                 modal.setAttribute('aria-modal', 'true');
                 modal.removeAttribute('aria-hidden');
             }
+
+            // Set timeout to close popup after 5 seconds
+            setTimeout(closePopup, 5000);
         }
     }
 });
@@ -17,7 +19,6 @@ function closePopup() {
     const modal = document.getElementById('popup');
     if (modal) {
         modal.classList.remove('show');
-        modal.style.display = 'none';
         modal.setAttribute('aria-hidden', 'true');
         modal.removeAttribute('aria-modal');
     }
