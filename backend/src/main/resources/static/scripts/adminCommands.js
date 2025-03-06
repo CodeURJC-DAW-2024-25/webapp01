fae939f687765998ee4be5105dd02de200ae0150
PortiESP <47105148+PortiESP@users.noreply.github.com>
2025-03-06 19:24:13 +0100

    fix: admin buttons

--- backend/src/main/resources/static/scripts/adminCommands.js
+++ backend/src/main/resources/static/scripts/adminCommands.js
@@ -2,63 +2,68 @@
 const CSRF_TOKEN = document.querySelector('meta[name="_csrf"]').content;
 const CSRF_HEADER = document.querySelector('meta[name="_csrf_header"]').content;
 
-// Handle post card clicks to navigate to the post's page
-const posts = [...document.querySelectorAll('[data-post-card]')];
-posts.forEach(post => {
-    const postId = post.getAttribute('data-post-id');
-    post.addEventListener('click', (e) => {
-        if (e.target !== e.currentTarget) return;
-        location.href = `/posts/${postId}`;
+
+const setPostsEvents = () => {
+    // Handle post card clicks to navigate to the post's page
+    const posts = [...document.querySelectorAll('[data-post-card]')];
+    posts.forEach(post => {
+        const postId = post.getAttribute('data-post-id');
+        post.addEventListener('click', (e) => {
+            if (e.target !== e.currentTarget) return;
+            location.href = `/posts/${postId}`;
+        });
     });
-});
 
-// Delete user buttons
-const $deleteUserButtons = [...document.querySelectorAll('[data-admin-command="user-delete"]')];
-$deleteUserButtons.forEach($deleteUserButton => {
-    $deleteUserButton.addEventListener('click', (e) => {
-        e.stopPropagation();
+    // Delete post buttons
+    const $deletePostButtons = [...document.querySelectorAll('[data-admin-command="post-delete"]')];
+    $deletePostButtons.forEach($deletePostButton => {
+        $deletePostButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
 
-        const confirmDelete = confirm('¿Estás seguro de que deseas eliminar este usuario?');
-        const uid = e.target.getAttribute('data-user-id');
+            const confirmDelete = confirm('¿Estás seguro de que deseas eliminar este post?');
+            const uid = e.target.getAttribute('data-post-id');
 
-        if (confirmDelete) {
-            fetch(`/admin/user/${uid}`, {
-                method: 'DELETE',
-                headers: {
-                    [CSRF_HEADER]: CSRF_TOKEN
-                }
-            })
-            .then(() => {
-                window.location.reload();
-            });
-        }
+            if (confirmDelete) {
+                fetch(`/admin/post/${uid}`, {
+                    method: 'DELETE',
+                    headers: {
+                        [CSRF_HEADER]: CSRF_TOKEN
+                    }
+                })
+                    .then(() => {
+                        window.location.reload();
+                    });
+            }
+        });
     });
-});
+}
 
-// Delete post buttons
-const $deletePostButtons = [...document.querySelectorAll('[data-admin-command="post-delete"]')];
-$deletePostButtons.forEach($deletePostButton => {
-    $deletePostButton.addEventListener('click', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
 
-        const confirmDelete = confirm('¿Estás seguro de que deseas eliminar este post?');
-        const uid = e.target.getAttribute('data-post-id');
+const setUsersEvents = () => {
+    // Delete user buttons
+    const $deleteUserButtons = [...document.querySelectorAll('[data-admin-command="user-delete"]')];
+    $deleteUserButtons.forEach($deleteUserButton => {
+        $deleteUserButton.addEventListener('click', (e) => {
+            e.stopPropagation();
 
-        if (confirmDelete) {
-            fetch(`/admin/post/${uid}`, {
-                method: 'DELETE',
-                headers: {
-                    [CSRF_HEADER]: CSRF_TOKEN
-                }
-            })
-            .then(() => {
-                window.location.reload();
-            });
-        }
+            const confirmDelete = confirm('¿Estás seguro de que deseas eliminar este usuario?');
+            const uid = e.target.getAttribute('data-user-id');
+
+            if (confirmDelete) {
+                fetch(`/admin/user/${uid}`, {
+                    method: 'DELETE',
+                    headers: {
+                        [CSRF_HEADER]: CSRF_TOKEN
+                    }
+                })
+                    .then(() => {
+                        window.location.reload();
+                    });
+            }
+        });
     });
-});
-
+}
 
 
 let loadedUsers = 0;
@@ -68,6 +73,7 @@ const fetchUsersTable = async () => {
     const data = await res.text();
     document.querySelector('[data-table="admin-users"]').innerHTML = data;
     if (firstLoadUsers) setUserButtonListener();
+    setUsersEvents();
 }
 
 let loadedPosts = 0;
@@ -77,6 +83,7 @@ const fetchPostsTable = async () => {
     const data = await res.text();
     document.querySelector('[data-table="admin-posts"]').innerHTML = data;
     if (firstLoadPosts) setPostButtonListener();
+    setPostsEvents();
 }
 
 const firstLoadUsers = true
