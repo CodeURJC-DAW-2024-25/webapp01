1a6c4d3c28104cc342137030bf5795272af67b0b
CuB1z <cub1zdev@gmail.com>
2025-03-08 09:33:07 +0100

    Refactor: refactor post fetching logic to use fetchData and add createHTMLPost function

--- backend/src/main/resources/static/scripts/fetchPosts.js
+++ backend/src/main/resources/static/scripts/fetchPosts.js
@@ -1,8 +1,8 @@
+import { fetchData } from './services/fetchService.js';
+import { createHTMLPost } from './services/uiService.js';
+
 const postsContainer = document.querySelector('#posts-preview-container');
 const loadMoreButton = document.querySelector('#load-more-button');
-
-const CSRF_TOKEN = document.querySelector('meta[name="_csrf"]').content;
-const CSRF_HEADER = document.querySelector('meta[name="_csrf_header"]').content;
 
 const POSTS_SIZE = 4;
 
@@ -16,20 +16,9 @@ async function loadPosts() {
     if (loading || isEnd) return;
     loading = true;
 
-    // Fetch for posts from the server
-    const response = await fetch(`/api/posts?page=${currentPage}&size=${POSTS_SIZE}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            [CSRF_HEADER]: CSRF_TOKEN
-        }
-    });
-
-    if (!response.ok) throw new Error('Failed to load posts');
-
-    // Parse the response
-    const data = await response.json();
-
-    console.log(data);
+    // Fetch posts from the server
+    const endpoint = `/posts?page=${currentPage}&size=${POSTS_SIZE}`;
+    const data = await fetchData(endpoint, 'GET', { cacheData: false });
 
     // Insert posts to the DOM
     data.posts.forEach(post => {
@@ -44,33 +33,6 @@ async function loadPosts() {
     if (data.isLastPage) {
         loadMoreButton.remove();
     }
-}
-
-function createHTMLPost(post) {
-    return `
-    <a class="post extended" href="/posts/${post.id}">
-        <i class="bi bi-box-arrow-up-right icon"></i>
-        <div class="post-top">
-            <h2 class="post-title">${post.title}</h2>
-            <p class="post-description">Descubre cómo puedes ahorrar dinero en tus compras de supermercado con SaveX.</p>
-            <div class="post-metadata">
-                <div class="tags-container">
-                    ${post.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
-                </div>
-            </div>
-        </div>
-        <div class="post-bottom">
-            <div class="time-container">
-                <span class="date">${post.date}</span>
-                <span class="separator">•</span>
-                <span class="read-time">${post.readingTime}</span>
-            </div>
-            <div class="img-container">
-                <img src="/api/posts/${post.id}/banner" alt="${post.title}">
-            </div>
-        </div>
-    </a>
-    `;
 }
 
 // Load the initial posts
