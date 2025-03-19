8b3ead685f11bc45bbfba88c79fc70cf35187aa1
CuB1z <cub1zdev@gmail.com>
2025-03-19 19:07:40 +0100

    Refactor: Remove unused code

--- backend/src/main/java/es/daw01/savex/controller/AdminController.java
+++ backend/src/main/java/es/daw01/savex/controller/AdminController.java
@@ -1,19 +1,12 @@
 package es.daw01.savex.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import es.daw01.savex.DTOs.UserDTO;
-import es.daw01.savex.DTOs.posts.PostDTO;
-import es.daw01.savex.model.UserType;
 import es.daw01.savex.components.ControllerUtils;
-import es.daw01.savex.service.PostService;
-import es.daw01.savex.service.UserService;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,30 +14,16 @@ public class AdminController {
 
     // --- Services ---
     @Autowired
-    private UserService userService;
-
-    @Autowired
-    private PostService postService;
-
-    @Autowired
     private ControllerUtils controllerUtils;
 
     // --- Methods ---
-    @GetMapping("")
+    @GetMapping({ "", "/" })
     public String getAdminPage(Model model) {
         // Add user data to the model
         controllerUtils.addUserDataToModel(model);
 
-        List<PostDTO> postDetails = postService.getPostsDTO(postService.findAll());
-        List<UserDTO> userDetails = userService.getUsersDTO(userService.findAllByRole(UserType.USER));
-
         // Add data to the model
         model.addAttribute("title", "SaveX - Panel de administración");
-        model.addAttribute("posts", postDetails);
-        model.addAttribute("users", userDetails);
-
-        // model.addAttribute("comments", commentService.findAll());
-
         return "admin";
     }
 }
