e666f50d438feb3ab52da006930a91285341d982
CuB1z <cub1zdev@gmail.com>
2025-03-22 19:03:01 +0100

    feat: Update UserMapper to include email and username mapping, and hash password on user creation

--- backend/src/main/java/es/daw01/savex/DTOs/users/UserMapper.java
+++ backend/src/main/java/es/daw01/savex/DTOs/users/UserMapper.java
@@ -1,5 +1,6 @@
 package es.daw01.savex.DTOs.users;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.mapstruct.AfterMapping;
@@ -8,6 +9,8 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import es.daw01.savex.model.User;
+import es.daw01.savex.model.UserType;
+import es.daw01.savex.utils.HashUtils;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -19,7 +22,9 @@ public interface UserMapper {
     List<PrivateUserDTO> toPrivateUserDTOs(List<User> users);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "email", source = "createUserRequest.email")
+    @Mapping(target = "name", source = "createUserRequest.username")
+    @Mapping(target = "username", source = "createUserRequest.username")
     @Mapping(target = "hashedPassword", ignore = true)
     @Mapping(target = "comments", ignore = true)
     @Mapping(target = "avatar", ignore = true)
@@ -28,10 +33,10 @@ public interface UserMapper {
 
     @AfterMapping
     default void afterMapping(CreateUserRequest createUserRequest, @MappingTarget User user) {
-        user.setName(createUserRequest.username());
-        user.setUsername(createUserRequest.username());
-
-        
+        user.setHashedPassword(HashUtils.hashPassword(createUserRequest.password()));
+        user.setRole(UserType.USER);
+        user.setAvatar(null);
+        user.setComments(new ArrayList<>());
     }
 
 
