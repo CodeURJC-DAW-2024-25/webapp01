d175c0f0591836ef115d75dd5f43004d61f2ac79
CuB1z <cub1zdev@gmail.com>
2025-04-03 20:32:55 +0200

    Feature: Refactor CTA component to use AuthService for authentication state

--- frontend/src/app/components/shared/cta/cta.component.ts
+++ frontend/src/app/components/shared/cta/cta.component.ts
@@ -1,12 +1,21 @@
-import { Component, inject } from "@angular/core";
+import { AuthService } from "@/services/auth.service";
+import { GlobalUser } from "@/types/User";
+import { Component, inject, OnInit } from "@angular/core";
 import { UserDataService } from "@services/user-data.service";
 
 @Component({
-  selector: "app-cta",
-  templateUrl: "./cta.component.html",
-  styleUrl: "./cta.component.css"
+	selector: "app-cta",
+	templateUrl: "./cta.component.html",
+	styleUrl: "./cta.component.css"
 })
-export class CtaComponent {
-  userData: UserDataService = inject(UserDataService);
+export class CtaComponent implements OnInit {
+	isAuthenticated = false;
 
+	private authService = inject(AuthService);
+
+	ngOnInit(): void {
+		this.authService.globalUser$.subscribe((user: GlobalUser | null) => {
+			this.isAuthenticated = !!user?.isAuthenticated;
+		});
+	}
 }
